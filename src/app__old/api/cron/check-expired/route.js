223ebfcebed68e4a563579827e93831f8ea5550c
Citaviriyo <citaviriyo@gmail.com>
2025-12-20 13:22:10 +0700

    chore: remove legacy src folder to fix tailwind build

--- src/app__old/api/cron/check-expired/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({
-    ok: true,
-    message: "check-expired route is alive (app router)",
-  });
-}
